cfad81e53ea4254a117f23151dc716a0896a2c44
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-27 20:50:36 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -83,7 +83,8 @@ function runSetupCommands() {
     "bun add react-dropzone",
     "bun install react-dropzone @tanstack/react-table",
     "bun add jspdf html2canvas",
-    "bun add @types/jspdf @types/html2canvas -D"
+    "bun add @types/jspdf @types/html2canvas -D",
+    "bun install jspdf html2canvas"
 ];
     
     if (commands.length === 0) {
